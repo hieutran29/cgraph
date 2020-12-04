170ccabdb5a11a31e29914575df5f5c033ef9366
hieutran29 <hieu.xteen28@gmail.com>
2020-12-04 20:10:06 +0700

    add cgraph_topological_sorting()

--- structural_properties.c
+++ structural_properties.c
@@ -49,3 +49,37 @@ int cgraph_is_dag(const cgraph_t *graph, bool *res) {
   }
   return 0;
 }
+
+int cgraph_topological_sorting(const cgraph_t *graph,
+                               cgraph_ivec_t *res,
+                               cgraph_neimode_t mode)
+{
+  cgraph_ivec_init(res, 0);
+  cgraph_neimode_t mode2;
+  if (mode == CGRAPH_OUT)
+    mode2 = CGRAPH_IN;
+  if (mode == CGRAPH_IN)
+    mode2 = CGRAPH_OUT;
+  cgraph_ivec_t dgree = cgraph_ivec_create();
+  cgraph_degree_all(graph, &dgree, mode2, true);
+  CGRAPH_INTEGER ressize = 0;
+  bool check = true;
+  while (check)
+  {
+    CGRAPH_INTEGER indx = 0;
+    cgraph_ivec_t nei = cgraph_ivec_create();
+    while (dgree[indx] != 0 && indx < graph->n)
+      indx++;
+    if (indx == graph->n)
+      check = false;
+    else
+    {
+      dgree[indx]--;
+      cgraph_neighbors(graph, &nei, indx, mode);
+      for (CGRAPH_INTEGER j = 0; j < cgraph_ivec_size(nei); j++)
+        dgree[nei[j]]--;
+      cgraph_ivec_push_back(res, indx);
+    }
+  }
+  return 0;
+}
