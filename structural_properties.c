973e6ad9d308fb24861c112e5b8b67ece6034d18
hieutran29 <hieu.xteen28@gmail.com>
2020-12-11 23:35:05 +0700

    <structural_properties.c>: my implementation to find shortest path between 2 vertices

--- structural_properties.c
+++ structural_properties.c
@@ -7,6 +7,8 @@
 #include "cgraph_paths.h"
 #include "cgraph_topology.h"
 
+#include <string.h>
+
 int cgraph_is_dag(const cgraph_t *graph, bool *res) {
   *res = true;
 
@@ -82,5 +84,104 @@ int cgraph_topological_sorting(const cgraph_t *graph,
       cgraph_ivec_push_back(res, indx);
     }
   }
+  return 0;
+}
+#include <stdio.h>
+#include <stdlib.h>
+int cgraph_get_shortest_path_dijkstra(const cgraph_t *graph,
+        cgraph_ivec_t *vertices,
+        cgraph_ivec_t *edges,
+        CGRAPH_INTEGER from,
+        CGRAPH_INTEGER to,
+        const double *weights,
+        cgraph_neimode_t mode) {
+
+  int index = 0;
+
+  double total_weight[graph->n];
+  CGRAPH_INTEGER tracking[graph->n];
+  CGRAPH_INTEGER passed[graph->n];    /* is this node passed or not? */
+
+  for(int i = 0; i < graph->n; i++) {
+    total_weight[i] = (double) 1000000007;
+    tracking[i] = -1;
+    passed[i] = 0;
+  }
+  
+  cgraph_iqueue_t bfs = cgraph_iqueue_create();
+  cgraph_iqueue_enqueue(bfs, from);
+
+  cgraph_ivec_t nei = cgraph_ivec_create();
+  /* base step */
+  cgraph_neighbors(graph, &nei, from, mode);
+  total_weight[from] = 0;
+  for(int i = 0; i < cgraph_ivec_size(nei); i++) {
+    for(index = 0; index < graph->n; index++) {
+      if(graph->from[index] == from && graph->to[index] == nei[i]) {
+        total_weight[ nei[i] ] = weights[index];
+        break;
+      }
+    }
+    tracking[ nei[i] ] = from;
+  }
+
+  while(!cgraph_iqueue_empty(bfs)) {
+    CGRAPH_INTEGER out;
+    cgraph_iqueue_poll(bfs, &out);
+    passed[out] = 1;
+
+    cgraph_neighbors(graph, &nei, out, mode);
+    for(int i = 0; i < cgraph_ivec_size(nei); i++) {
+      /* find weight's index */
+      for(index = 0; index < cgraph_ivec_size(graph->from); index++) {
+        if(graph->from[index] == out && graph->to[index] == nei[i]) {
+          break;
+        }
+      }
+      if(!passed[ nei[i] ] || 
+          weights[index] + total_weight[out] < total_weight[ nei[i] ]) {
+
+        cgraph_iqueue_enqueue(bfs, nei[i]);
+
+        if(weights[index] + total_weight[out] < total_weight[ nei[i] ]) {
+          total_weight[ nei[i] ] = weights[index] + total_weight[out];
+          tracking[ nei[i] ] = out;
+        }
+      }
+    }
+  }
+
+  CGRAPH_INTEGER i = to;
+  while(true) {
+    if(i == from) {
+      cgraph_ivec_push_back(vertices, from);
+      break;
+    }
+    else {
+      cgraph_ivec_push_back(vertices, i);
+      i = tracking[i];
+    }
+  }
+
+  /* we need to reverse the vector vertices */
+  for(int i = 0; i < cgraph_ivec_size(*vertices) / 2; i++) {
+    int swap = (*vertices)[i];
+    (*vertices)[i] = (*vertices)[cgraph_ivec_size(*vertices) - i - 1];
+    (*vertices)[cgraph_ivec_size(*vertices) - i - 1] = swap;
+  }
+
+  /* vector edges */
+  for(int i = 0; i < cgraph_ivec_size(*vertices) - 1; i++) {
+      for(index = 0; index < cgraph_ivec_size(graph->from); index++) {
+        if(graph->from[index] == (*vertices)[i] && graph->to[index] == (*vertices)[i+1]) {
+          cgraph_ivec_push_back(edges, index);
+          break;
+        }
+      }
+  }
+
+  cgraph_ivec_free(&nei);
+  cgraph_iqueue_free(&bfs);
+
   return 0;
 }
